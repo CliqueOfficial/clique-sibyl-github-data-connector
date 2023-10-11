ac5b43acb561f3a3f08a1e79021d993ea9cca0d7
Martinprobu <274322055@qq.com>
2023-10-11 12:17:44 +0800

    remove log
    remove log

--- connectors/SibylGithubDataConnector/src/github.rs
+++ connectors/SibylGithubDataConnector/src/github.rs
@@ -271,10 +271,8 @@ impl DataConnector for GithubConnector {
                 );
                 let github_id_hash: String;
                 let github_username: String;
-
-                let simple_tls_client_params = format!("simple_tls_client_params: {:?}", query_user);
-                println!("{:?}", simple_tls_client_params);
-
+                // let simple_tls_client_params = format!("simple_tls_client_params: {:?}", query_user);
+                // println!("{:?}", simple_tls_client_params);
                 match simple_tls_client(GITHUB_API_HOST, &query_user, 443) {
                     Ok(r) => {
                         // let simple_tls_client_rsp = format!("simple_tls_client_rsp: {:?}", r);
