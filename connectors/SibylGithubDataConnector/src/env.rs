f81d340931bd7fcd99b4d7088d1b07002a1302d4
will-beta <i@will-beta.com>
2023-10-27 11:09:45 +0800

    fix private constant variables access issue

--- connectors/SibylGithubDataConnector/src/env.rs
+++ connectors/SibylGithubDataConnector/src/env.rs
@@ -1,6 +1,6 @@
-const SIGN_CLAIM_SGX_HOST: &'static str = "clique-sign-claim";
+pub const SIGN_CLAIM_SGX_HOST: &'static str = "clique-sign-claim";
 
 // Github GraphQL API
-const GITHUB_API_HOST: &'static str = "api.github.com";
-const GITHUB_GRAPHQL_SUFFIX: &'static str = "/graphql";
-const GITHUB_USER_SUFFIX: &'static str = "/user";
+pub const GITHUB_API_HOST: &'static str = "api.github.com";
+pub const GITHUB_GRAPHQL_SUFFIX: &'static str = "/graphql";
+pub const GITHUB_USER_SUFFIX: &'static str = "/user";
